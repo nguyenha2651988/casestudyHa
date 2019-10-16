a426d7db9cc7ee04b15eb8f4183808da7f5046b0
anhhaday <nguyenha26051988@gmail.com>
2019-10-16 16:12:08 +0700

    ok em oi

--- src/main/java/com/codegym/SpringBootWebApplication.java
+++ src/main/java/com/codegym/SpringBootWebApplication.java
@@ -1,6 +1,8 @@
 package com.codegym;
 
 
+import com.codegym.service.NewsService;
+import com.codegym.service.impl.NewsServiceImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -19,57 +22,32 @@ import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-//@EnableJpaRepositories("com.codegym.repository")
-//@EntityScan("com.codegym.model")
+@EnableJpaRepositories("com.codegym.repository")
+@EntityScan("com.codegym.model")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(SpringBootWebApplication.class);
-//    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class);
     }
 
 
-//    @Bean
-//    public NewsService newsService(){
-//        return new NewsServiceImpl();
-//    }
+    @Bean
+    public NewsService newsService(){
+        return new NewsServiceImpl();
+    }
 
-//    private ApplicationContext applicationContext;
-//
-//    @Override
-//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-//        this.applicationContext = applicationContext;
-//    }
-//
-//
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-//        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/view");
+        templateResolver.setPrefix("/WEB-INF/view/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
-//
-//
-//    @Bean
-//    public TemplateEngine templateEngine() {
-//        TemplateEngine templateEngine = new SpringTemplateEngine();
-//        templateEngine.setTemplateResolver(templateResolver());
-//        return templateEngine;
-//    }
-//
-//    @Bean
-//    public ThymeleafViewResolver viewResolver() {
-//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine((ISpringTemplateEngine) templateEngine());
-//        viewResolver.setCharacterEncoding("UTF-8");
-//        return viewResolver;
-//    }
 
 }
--- src/main/java/com/codegym/controller/HomeMapleStoryController.java
+++ src/main/java/com/codegym/controller/HomeMapleStoryController.java
@@ -1,8 +1,8 @@
 package com.codegym.controller;
 
-//import com.codegym.service.NewsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Pageable;
+import com.codegym.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,13 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeMapleStoryController {
 
-//    @Autowired
-//    private NewsService newsService;
+    @Autowired
+    private NewsService newsService;
 
     @GetMapping("/home")
-    public ModelAndView homeNews(){
-        ModelAndView modelAndView = new ModelAndView("/news/home");
-//        modelAndView.addObject("news",newsService.findAll(pageable));
+    public ModelAndView homeNews(Pageable pageable){
+        ModelAndView modelAndView = new ModelAndView("news/home");
+        modelAndView.addObject("news",newsService.findAll(pageable));
         return modelAndView;
     }
 }
